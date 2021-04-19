55061d00dd776f6f6faf161fd4a43eb9307f0ebd
jim879546213-deep <74955844+jim879546213-deep@users.noreply.github.com>
2021-04-19 15:51:08 +0800

    Update try_import_packages.py

--- try_import_packages.py
+++ try_import_packages.py
@@ -16,7 +16,7 @@ import torch
 device_obj = select_device('')
 half = device_obj.type != 'cpu'
 
-def return_model(obj_path = "pth\\best_adj.pt", cls_res_path = None,cls_next_path=None,vgg_16_path = None):
+def return_model(obj_path = "pth\\best_adj.pt", cls_res_path = None,cls_next_path=None,vgg_11_path = None):
     '''
     input: 物件偵測模型參數位址, 分類模型參數位址
     return: 物件偵測模型, 分類模型
@@ -50,9 +50,9 @@ def return_model(obj_path = "pth\\best_adj.pt", cls_res_path = None,cls_next_pat
         model_cls.to(device_cls)
         model_cls.load_state_dict(torch.load(cls_next_path))
         model_cls.eval()
-    elif vgg_16_path is not None:
-        model_cls = models.vgg16_bn(pretrained=False)
-        model_cls.classifier = nn.Sequential(
+    elif vgg_11_path is not None:
+        model = models.vgg11_bn(pretrained=False)
+        model.classifier = nn.Sequential(
         nn.Linear(in_features=25088, out_features=4096, bias=True),
         nn.ReLU(inplace=True),
         nn.Dropout(p=0.5, inplace=False),
@@ -62,7 +62,7 @@ def return_model(obj_path = "pth\\best_adj.pt", cls_res_path = None,cls_next_pat
         nn.Linear(in_features=4096, out_features=1, bias=True),nn.Sigmoid()
           )
         model_cls.to(device_cls)
-        model_cls.load_state_dict(torch.load(vgg_16_path))
+        model_cls.load_state_dict(torch.load(vgg_11_path))
         model_cls.eval()
 
     return model_obj, model_cls
